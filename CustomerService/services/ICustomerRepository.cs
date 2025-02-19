63be023d862948f5cd5ab741ad156a4ddcc9921e
AdamAgerling <Alltidvakenhehe@gmail.com>
2025-02-19 11:16:21 +0100

    Add Customer interface
    Move Customer class to separate file
    
    Add Dependency injection to CustomerService project

---
+++ CustomerService/services/ICustomerRepository.cs
@@ -0,0 +1,8 @@
+public interface ICustomerRepository
+{
+    IEnumerable<Customer> GetAll();
+    Customer GetById(int id);
+    void Add(Customer customer);
+    void Update(Customer customer);
+    void Delete(int id);
+};
